c759ff3b87b7e5e2da84794bf1e05d4e3958c95e
sl <slau.phys@gmail.com>
2024-11-14 09:58:45 -0500

    Delete config.json.py

--- config.json.py
+++
@@ -1,5 +0,0 @@
-{
-    "data_store_path": "/Users/joshua_liu/Dropbox/Sublime/py_codes/Github/NeuroAI-Daily-Arxiv/data_store",
-    "output_path": "/Users/joshua_liu/Dropbox/Sublime/py_codes/Github/NeuroAI-Daily-Arxiv/data_analysis",
-    "api_key": "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"
-}
